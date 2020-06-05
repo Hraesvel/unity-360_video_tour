61e1531850cbe687e4ac464ff3719b5aeddc51ed
Martin Smith <mcodesmith@gmail.com>
2020-06-05 14:13:32 -0700

    fixed handling action button

--- Assets/Scripts/RayInput.cs
+++ Assets/Scripts/RayInput.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Experimental.Input;
 using UnityEngine.XR;
@@ -10,17 +11,34 @@ public class RayInput : MonoBehaviour
     [SerializeField] private Controls ctrl;
     [SerializeField] private bool drawDebugLine;
     [SerializeField] private LayerMask uiLayerMask;
+    
 
     private Ray _ray;
     private RaycastHit _rayHit;
     private PointerEventData eventData;
+    private bool _aAction;
 
-    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        ctrl.BasicCtrl.A.performed += start => AOnperformed();
+        ctrl.BasicCtrl.A.cancelled += stop => AOnCancel();
+        _aAction = false;
+    }
+    private void AOnperformed()
+    {
+        Debug.Log("PRESS");
+        _aAction = true;
+    }
+
+    private void AOnCancel()
+    {
+        Debug.Log("RELEASE");
+        _aAction = false;
+    }
 
     private void Start()
     {
-        ctrl.BasicCtrl.Enable();
-
         uiLayerMask = LayerMask.NameToLayer("UI");
         eventData = new PointerEventData(EventSystem.current);
         eventData.pointerId = 0;
@@ -28,7 +46,6 @@ public class RayInput : MonoBehaviour
         if (XRSettings.enabled)
         {
             Debug.Log("VR ON!");
-
             eventData.position = new Vector2(XRSettings.eyeTextureWidth / 2f,
                 XRSettings.eyeTextureHeight / 2f);
         }
@@ -54,9 +71,9 @@ public class RayInput : MonoBehaviour
 
         if (drawDebugLine)
             Debug.DrawLine(transform.position, transform.TransformDirection(Vector3.forward) * RayDistancce, Color.red);
-        
+
         Physics.Raycast(_ray, out _rayHit, RayDistancce);
-       
+
         if (_rayHit.transform == null)
         {
             LookAway();
@@ -89,13 +106,13 @@ public class RayInput : MonoBehaviour
 
     private void CheckHit()
     {
-        if (ctrl.BasicCtrl.A.enabled && eventData.pointerEnter != null)
+        if (_aAction && eventData.pointerEnter != null)
         {
             eventData.pointerPressRaycast = eventData.pointerCurrentRaycast;
             eventData.pointerPress =
                 ExecuteEvents.ExecuteHierarchy(eventData.pointerEnter, eventData, ExecuteEvents.pointerDownHandler);
         }
-        else if (!ctrl.BasicCtrl.A.enabled)
+        else if (!_aAction)
         {
             if (eventData.pointerPress != null)
                 ExecuteEvents.ExecuteHierarchy(eventData.pointerPress, eventData, ExecuteEvents.pointerUpHandler);
@@ -118,18 +135,13 @@ public class RayInput : MonoBehaviour
 
     private void OnEnable()
     {
-        // ctrl.BasicCtrl.A.performed += AOnperformed;
-        ctrl.BasicCtrl.A.Enable();
+        ctrl.Enable();
     }
 
     private void OnDisable()
     {
-        // ctrl.BasicCtrl.A.performed -= AOnperformed;
-        ctrl.BasicCtrl.A.Disable();
+        ctrl.Disable();
     }
 
-    // private void AOnperformed(InputAction.CallbackContext obj)
-    // {
-    //     // CheckHit();
-    // }
+ 
 }
