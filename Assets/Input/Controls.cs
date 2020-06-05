7f64cf7ece0345548c05208106d8dbee94ae14c7
Martin Smith <mcodesmith@gmail.com>
2020-06-05 14:11:50 -0700

    remove (temporary) unused input

--- Assets/Input/Controls.cs
+++ Assets/Input/Controls.cs
@@ -1,35 +1,21 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Controls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/Controls.inputactions'
 
 using System;
+using UnityEngine;
 using UnityEngine.Experimental.Input;
-using Object = UnityEngine.Object;
+
 
 [Serializable]
 public class Controls : InputActionAssetReference
 {
-    // BasicCtrl
-    private InputActionMap m_BasicCtrl;
-    private InputAction m_BasicCtrl_A;
-    private bool m_Initialized;
-
     public Controls()
     {
     }
-
     public Controls(InputActionAsset asset)
         : base(asset)
     {
     }
-
-    public BasicCtrlActions BasicCtrl
-    {
-        get
-        {
-            if (!m_Initialized) Initialize();
-            return new BasicCtrlActions(this);
-        }
-    }
-
+    private bool m_Initialized;
     private void Initialize()
     {
         // BasicCtrl
@@ -37,62 +23,43 @@ public class Controls : InputActionAssetReference
         m_BasicCtrl_A = m_BasicCtrl.GetAction("A");
         m_Initialized = true;
     }
-
     private void Uninitialize()
     {
         m_BasicCtrl = null;
         m_BasicCtrl_A = null;
         m_Initialized = false;
     }
-
     public void SetAsset(InputActionAsset newAsset)
     {
         if (newAsset == asset) return;
         if (m_Initialized) Uninitialize();
         asset = newAsset;
     }
-
     public override void MakePrivateCopyOfActions()
     {
-        SetAsset(Object.Instantiate(asset));
+        SetAsset(ScriptableObject.Instantiate(asset));
     }
-
+    // BasicCtrl
+    private InputActionMap m_BasicCtrl;
+    private InputAction m_BasicCtrl_A;
     public struct BasicCtrlActions
     {
-        private readonly Controls m_Wrapper;
-
-        public BasicCtrlActions(Controls wrapper)
+        private Controls m_Wrapper;
+        public BasicCtrlActions(Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @A { get { return m_Wrapper.m_BasicCtrl_A; } }
+        public InputActionMap Get() { return m_Wrapper.m_BasicCtrl; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled { get { return Get().enabled; } }
+        public InputActionMap Clone() { return Get().Clone(); }
+        public static implicit operator InputActionMap(BasicCtrlActions set) { return set.Get(); }
+    }
+    public BasicCtrlActions @BasicCtrl
+    {
+        get
         {
-            m_Wrapper = wrapper;
-        }
-
-        public InputAction A => m_Wrapper.m_BasicCtrl_A;
-
-        public InputActionMap Get()
-        {
-            return m_Wrapper.m_BasicCtrl;
-        }
-
-        public void Enable()
-        {
-            Get().Enable();
-        }
-
-        public void Disable()
-        {
-            Get().Disable();
-        }
-
-        public bool enabled => Get().enabled;
-
-        public InputActionMap Clone()
-        {
-            return Get().Clone();
-        }
-
-        public static implicit operator InputActionMap(BasicCtrlActions set)
-        {
-            return set.Get();
+            if (!m_Initialized) Initialize();
+            return new BasicCtrlActions(this);
         }
     }
 }
